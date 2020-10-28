bde058e34fde73e2d3dc746641116e12073160c8
anojkumarmandal14 <73439314+anojkumarmandal14@users.noreply.github.com>
2020-10-28 14:24:33 +0530

    Add files via upload

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,5 +1,4 @@
-﻿using MVCAssignment.Helper;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,7 +17,6 @@ namespace MVCAssignment
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            ApiHelper.InitializeClient();
         }
     }
 }
